cd2c1c0a90bfec847ad66fbff1dd83ccd7ed68a7
WolcenOn <fhaldon@gmail.com>
2026-06-02 12:04:00 +0200

    Update ejemplo.js

--- packs/ejemplo.js
+++ packs/ejemplo.js
@@ -1,1 +1,422 @@
-prueba
+{
+  "app": "gestor-comidas",
+  "version": 2,
+  "exportedAt": "2026-06-02T10:03:06.465Z",
+  "ingredientFamilies": [
+    {
+      "id": "763c0492-e7b8-4eb6-80cd-91ed0ff7c265",
+      "name": "Verdura"
+    },
+    {
+      "id": "a8716c99-4595-45b7-9595-ceb68ce1df67",
+      "name": "Fruta"
+    },
+    {
+      "id": "7743481d-0d5a-42ee-b3d2-051f392baa5f",
+      "name": "Lácteos"
+    },
+    {
+      "id": "a6b42f68-17be-4c4c-bd1d-1b64e55f42d6",
+      "name": "Carne y pescado"
+    },
+    {
+      "id": "6e637906-7107-44d1-a560-66e12fabfca5",
+      "name": "Despensa"
+    },
+    {
+      "id": "f505960c-d913-4e6e-a441-6d92a636ead9",
+      "name": "Congelados"
+    },
+    {
+      "id": "8d897525-2510-4423-8bb3-ec08cfef7e7f",
+      "name": "Otros"
+    }
+  ],
+  "ingredients": [
+    {
+      "id": "f92e245e-35ab-4e0a-b687-f218f5880253",
+      "name": "Salmorejo",
+      "qty": 1,
+      "unit": "ración",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "baa6f73c-4bb3-4e81-96ea-29399f699485",
+      "name": "Huevo",
+      "qty": 10,
+      "unit": "unidades",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "c6d14b24-bf0f-468a-8dd5-924d6b766409",
+      "name": "Jamón en tacos",
+      "qty": 80,
+      "unit": "g",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "daf5068e-6a78-4a3a-b5cf-6cdb1f21978c",
+      "name": "Pasta",
+      "qty": 0,
+      "unit": "g",
+      "available": false,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": [
+        {
+          "barcode": "",
+          "brand": "A granel",
+          "packageQty": 1000,
+          "packageUnit": "g",
+          "price": 1.5
+        }
+      ]
+    },
+    {
+      "id": "228b1045-2cf6-4e96-bdd3-f235c2ca7a12",
+      "name": "Calabacin",
+      "qty": 0,
+      "unit": "unidades",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "6a61e3ba-d40b-4ae9-8cff-687f585186a7",
+      "name": "Zanahoria",
+      "qty": 0,
+      "unit": "unidades",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "0b8694f2-efbe-48ee-97fe-b0fb95072fd0",
+      "name": "Espinacas",
+      "qty": 0,
+      "unit": "Bolsa",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "163d2792-5a12-430b-9866-7500b35a3efe",
+      "name": "Bacalao",
+      "qty": 2,
+      "unit": "Gramos",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "8999fb22-c2db-40d1-995b-3cfb6a42962b",
+      "name": "Queso cuña García Vaquero",
+      "qty": 0,
+      "unit": "unidades",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "a84c36ff-e515-4201-8277-77d1468ca6ad",
+      "name": "Queso Cremoso Light",
+      "qty": 0,
+      "unit": "unidades",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "3741921d-9b79-4de8-be09-d7bceb198f61",
+      "name": "Carne picada",
+      "qty": 0,
+      "unit": "Grms",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "7b89a8ed-26ba-412d-8a1c-96dab2be1846",
+      "name": "Tomate frito",
+      "qty": 0,
+      "unit": "Bote",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "874bd3ef-dc64-4551-aa76-1d05755aa05a",
+      "name": "Pechuga Pollo",
+      "qty": 0,
+      "unit": "Gramos",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "184623e3-aba9-4fd7-ab0c-6515f0e69317",
+      "name": "Salmón",
+      "qty": 0,
+      "unit": "Gramos",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "e5b73d75-65ea-4180-a2e5-2057f583000b",
+      "name": "Bacalao",
+      "qty": 0,
+      "unit": "Salmon",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "02267bc2-b260-4b0c-80b2-b0cad76575c4",
+      "name": "Ajos",
+      "qty": 0,
+      "unit": "Gramos",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "4e9e5dc2-8fbb-4112-b3d1-eafd54169c08",
+      "name": "Cebollas",
+      "qty": 0,
+      "unit": "Unidad",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "41c03c6b-ea28-46cf-a324-372601e3c0ab",
+      "name": "Pan Bimbo",
+      "qty": 0,
+      "unit": "Rebanadas",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    },
+    {
+      "id": "9ff1c768-08e2-46b2-a65f-9e62709eb3fe",
+      "name": "Jamón duroc",
+      "qty": 0,
+      "unit": "Bandeja",
+      "available": true,
+      "familyId": "",
+      "approxPrice": 0,
+      "products": []
+    }
+  ],
+  "dishPacks": [
+    {
+      "id": "6fc57213-41c4-42c4-a6d9-cd2232459836",
+      "name": "Sin clasificar"
+    },
+    {
+      "id": "1f28472a-f5ea-445a-b9c4-5e8fba80a7ef",
+      "name": "Dieta mediterránea"
+    },
+    {
+      "id": "bd2831b3-e4a6-47de-9847-23f200f7f49e",
+      "name": "Vegano"
+    },
+    {
+      "id": "003077a2-9254-42e8-ad98-c87889cf9167",
+      "name": "Diabetes"
+    },
+    {
+      "id": "0c69caa4-be48-45ed-be8d-3bff00373c9c",
+      "name": "Cenas ligeras"
+    }
+  ],
+  "dishes": [
+    {
+      "id": "acbd5f56-d6f8-49c6-9b96-3e2d2b3106b8",
+      "name": "Salmorejo completo",
+      "qty": 0,
+      "unit": "raciones",
+      "notes": "Compuesto por salmorejo, huevo cocido y jamón en tacos.",
+      "packId": "",
+      "category": "",
+      "tags": [],
+      "prepTime": "",
+      "difficulty": "",
+      "approxPrice": 0,
+      "recipe": [
+        {
+          "ingredientId": "f92e245e-35ab-4e0a-b687-f218f5880253",
+          "qty": 1
+        },
+        {
+          "ingredientId": "baa6f73c-4bb3-4e81-96ea-29399f699485",
+          "qty": 1
+        },
+        {
+          "ingredientId": "c6d14b24-bf0f-468a-8dd5-924d6b766409",
+          "qty": 40
+        }
+      ]
+    },
+    {
+      "id": "22bdeaab-701f-452d-9714-4b6d14266394",
+      "name": "Puré",
+      "qty": 0,
+      "unit": "raciones",
+      "notes": "",
+      "packId": "",
+      "category": "",
+      "tags": [],
+      "prepTime": "",
+      "difficulty": "",
+      "approxPrice": 0,
+      "recipe": [
+        {
+          "ingredientId": "228b1045-2cf6-4e96-bdd3-f235c2ca7a12",
+          "qty": 1
+        },
+        {
+          "ingredientId": "6a61e3ba-d40b-4ae9-8cff-687f585186a7",
+          "qty": 1
+        },
+        {
+          "ingredientId": "0b8694f2-efbe-48ee-97fe-b0fb95072fd0",
+          "qty": 0.33
+        }
+      ]
+    },
+    {
+      "id": "8ef9f72c-4c15-4b26-aa10-7c0dce4d6f1f",
+      "name": "Boloñesa",
+      "qty": 0,
+      "unit": "raciones",
+      "notes": "",
+      "packId": "",
+      "category": "",
+      "tags": [],
+      "prepTime": "",
+      "difficulty": "",
+      "approxPrice": 0,
+      "recipe": [
+        {
+          "ingredientId": "3741921d-9b79-4de8-be09-d7bceb198f61",
+          "qty": 150
+        },
+        {
+          "ingredientId": "7b89a8ed-26ba-412d-8a1c-96dab2be1846",
+          "qty": 0.4
+        },
+        {
+          "ingredientId": "4e9e5dc2-8fbb-4112-b3d1-eafd54169c08",
+          "qty": 0.25
+        },
+        {
+          "ingredientId": "02267bc2-b260-4b0c-80b2-b0cad76575c4",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "id": "cba5dc91-25bf-4731-aac8-5c49281a5e17",
+      "name": "Tortilla Francesa",
+      "qty": 0,
+      "unit": "raciones",
+      "notes": "",
+      "packId": "",
+      "category": "",
+      "tags": [],
+      "prepTime": "",
+      "difficulty": "",
+      "approxPrice": 0,
+      "recipe": []
+    },
+    {
+      "id": "f33fee53-77ad-424f-802c-a3e21b012ca1",
+      "name": "Salmón",
+      "qty": 0,
+      "unit": "raciones",
+      "notes": "",
+      "packId": "",
+      "category": "",
+      "tags": [],
+      "prepTime": "",
+      "difficulty": "",
+      "approxPrice": 0,
+      "recipe": [
+        {
+          "ingredientId": "184623e3-aba9-4fd7-ab0c-6515f0e69317",
+          "qty": 200
+        }
+      ]
+    }
+  ],
+  "favoriteIds": [],
+  "familyMembers": [
+    {
+      "id": "24f5a34c-d9d8-40b9-9dc3-e492d8604e6a",
+      "name": "Virginia"
+    },
+    {
+      "id": "0415870f-86dd-41ff-b6d5-6687c8f60bd4",
+      "name": "Fran"
+    }
+  ],
+  "mealTypes": [
+    {
+      "id": "8602ef92-d218-4fc5-b40d-8acb787960e4",
+      "name": "Comida"
+    },
+    {
+      "id": "1064aff5-0887-4dca-a518-6f3cc1423e61",
+      "name": "Cena"
+    }
+  ],
+  "weeks": [
+    {
+      "id": "34df78c5-b913-4e6e-bcff-fc87ac7682fe",
+      "name": "Semana actual",
+      "plan": {
+        "Lunes__8602ef92-d218-4fc5-b40d-8acb787960e4__24f5a34c-d9d8-40b9-9dc3-e492d8604e6a": [
+          "8ef9f72c-4c15-4b26-aa10-7c0dce4d6f1f"
+        ],
+        "Lunes__8602ef92-d218-4fc5-b40d-8acb787960e4__0415870f-86dd-41ff-b6d5-6687c8f60bd4": [
+          "8ef9f72c-4c15-4b26-aa10-7c0dce4d6f1f"
+        ],
+        "Lunes__1064aff5-0887-4dca-a518-6f3cc1423e61__24f5a34c-d9d8-40b9-9dc3-e492d8604e6a": [
+          "8ef9f72c-4c15-4b26-aa10-7c0dce4d6f1f"
+        ],
+        "Lunes__1064aff5-0887-4dca-a518-6f3cc1423e61__0415870f-86dd-41ff-b6d5-6687c8f60bd4": [
+          "8ef9f72c-4c15-4b26-aa10-7c0dce4d6f1f"
+        ]
+      },
+      "createdAt": "2026-06-01T08:50:17.960Z",
+      "isTypical": false
+    }
+  ],
+  "activeWeekId": "34df78c5-b913-4e6e-bcff-fc87ac7682fe",
+  "typicalWeekPlan": null
+}
